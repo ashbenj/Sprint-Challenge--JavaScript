e33f4d871003e5f7aa3cd81d27ff9d87fa61c522
Asha <ashalbenjamin@gmail.com>
2019-05-31 21:00:02 -0500

    finished-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -55,7 +55,12 @@ console.log(dino1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+let roar = {};
+	dino1.speak = function(line) {
+		console.log("RAWERSRARARWERSARARARRRR!");
+	}
+	dino1.speak("RAWERSRARARWERSARARARRRR!");
 
 
 // ==== Arrays ====
@@ -73,11 +78,23 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array.*/
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
+let universities = [];
+for (let i=0; i<graduates.length; i++){
+	universities.push(graduates[i].university);
+}
+universities.sort();
+console.log(universities);
+
+/*Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
-console.log(universities)
+for (let i=0; i<graduates.length; i++){
+	universities.push(graduates[i].university);
+}
+universities.sort();
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -85,13 +102,23 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
+
 const contactInfo = [];
-console.log(contactInfo);
+graduates.forEach(function(graduates) {
+	contactInfo.push(`${graduates.first_name}  ${graduates.email}`);
+});
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+
 const uni = [];
+	for(i = 0; i<graduates.length; i++){
+		graduates[i];
+		if(university === uni)
+		uni.push(graduates[i].university)
+	}
 console.log(uni);
+
 
 
 // ==== ADVANCED Array Methods ====
